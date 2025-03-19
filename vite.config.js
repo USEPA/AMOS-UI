0f4969008d7c64c188bf912630bd792e35d80fdd
Valery Tkachenko <tkachenko.valery@gmail.com>
2025-03-19 18:07:37 -0400

    dependenckies and base in build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/amos',
+    base: '/amos/',
     plugins: [vue()],
     resolve: {
         alias: [
