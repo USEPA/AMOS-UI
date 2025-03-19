c7a73b8084924a06c3afad5c871cac3967840c07
Valery Tkachenko <tkachenko.valery@gmail.com>
2025-03-19 12:43:40 -0400

    base path changed

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      {find: "@", replacement: "/src/"}
-    ]
-  },
-  // disables an error message that appears randomly and for no known reason
-  server: {
-    hmr: {
-      overlay: false
+    base: '/amos',
+    plugins: [vue()],
+    resolve: {
+        alias: [
+            {find: "@", replacement: "/src/"}
+        ]
+    },
+    // disables an error message that appears randomly and for no known reason
+    server: {
+        hmr: {
+            overlay: false
+        }
     }
-  }
 })
